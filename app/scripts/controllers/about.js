e57163a00201fbc2092a16d4a65ed40695fa598a
Max Humberstone <maxhumberstonedesigns@gmail.com>
2016-09-26 09:15:06 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularSiteApp
+ */
+angular.module('angularSiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularSiteApp
+ */
+angular.module('angularSiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
