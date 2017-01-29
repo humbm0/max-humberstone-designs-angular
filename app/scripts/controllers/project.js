b53712b83f6aa0efdc54b71904a3c6599626ec22
Max Humberstone <maxhumberstonedesigns@gmail.com>
2017-01-29 16:36:45 +0000

    related projects functionality

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -8,14 +8,23 @@
  * Controller of the angularSiteApp
  */
 angular.module('angularSiteApp')
-  .controller('ProjectCtrl', function ($scope, Projects, $routeParams, $filter, $timeout) {
+  .controller('ProjectCtrl', function ($scope, Projects, $routeParams, $filter, $timeout, $window) {
 
     $scope.pageClass = 'page-project';
 
     Projects.all().then(function(data){
       var projects = data.data.projects;
+      $scope.relatedProjects = [];
       $scope.project = $filter('filter')(projects, {handle: $routeParams.projectId})[0];
-      $scope.relatedProjects = $filter('filter')(projects, {type: $scope.project.type});
+      $scope.data = $filter('filter')(projects, {type: $scope.project.type, handle: '!' + $routeParams.projectId});
+      angular.forEach($scope.data, function(project){
+        $scope.relatedProjects.push({
+          project: project,
+          order: 0.5 - $window.Math.random()
+        });
+      });
+
+
       console.log($scope.project);
       console.log($scope.relatedProjects);
     });
