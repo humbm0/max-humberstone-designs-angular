1f100ded0d9d300e3e0e2f51f9b1f4cccadbc649
Max Humberstone <maxhumberstonedesigns@gmail.com>
2017-04-12 22:55:26 +0100

    Yukon project page complete & home page hero fix

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -8,10 +8,25 @@
  * Controller of the angularSiteApp
  */
 angular.module('angularSiteApp')
-  .controller('ContactCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ContactCtrl', function ($scope, $http) {
+
+    $scope.success = false;
+    $scope.error = false;
+
+    $scope.sendMessage = function( input ) {
+      $http({
+          method: 'POST',
+          url: '../scripts/processForm.php',
+          data: input,
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      })
+      .success( function(data) {
+        if ( data.success ) {
+          $scope.success = true;
+        } else {
+          $scope.error = true;
+        }
+      } );
+    }
+
   });
