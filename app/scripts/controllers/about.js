f54ceaeb61b1bba3ac4e7f1ba03e4575324a16c2
Max Humberstone <maxhumberstonedesigns@gmail.com>
2017-08-05 09:25:15 +0100

    additional projects info

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularSiteApp
+ */
+angular.module('angularSiteApp')
+  .controller('AboutCtrl', function ($scope) {
+
+  });
